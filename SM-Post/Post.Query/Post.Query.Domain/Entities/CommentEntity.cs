58a95c10900eaa9f7f9a7ed5a66d3a5600d441ee
Jose Luis del Rio <e_jriome@mercadona.com>
2025-03-17 15:06:34 +0100

    event handling

--- SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -9,11 +9,11 @@ public class CommentEntity
     [Key]
     public Guid CommentId {get; set;}
     public required string Username {get; set;}
-    public required string CommentDate {get; set;}
+    public required DateTime CommentDate {get; set;}
     public required string Comment {get; set;}
     public bool Edited {get; set;}
     public Guid PostId { get; set;}
 
     [System.Text.Json.Serialization.JsonIgnore]
-    public virtual required PostEntity Post {get; set;}
+    public virtual PostEntity Post {get; set;}
 }
